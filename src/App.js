67b4b397f530c1a5adcdaeea3c7c50c0df3c914a
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-09-17 10:15:44 +0530

    add form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-        <h1>Hello World</h1>
+const App = () => {
+  const APP_ID = '8d2c3366'
+  const APP_KEY = 'd9dd82d3f064130f4bc2777c203095c2'
+  const requestChicken = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  
+  return(
+    <div className='App'>
+      <form className='search-form'>
+        <input className='search-bar' type='text'/>
+        <button className='search-button' type='submit'>Search</button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

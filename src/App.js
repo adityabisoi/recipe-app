7bf1cd747a410b5c7e564fa79dc95967c945d71b
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-09-17 11:15:14 +0530

    fetch from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 
 const App = () => {
   const APP_ID = '8d2c3366'
   const APP_KEY = 'd9dd82d3f064130f4bc2777c203095c2'
-  const requestChicken = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
-  
+
+  useEffect(()=> {
+    getRecipe()
+  },[])
+
+  const getRecipe = () => {
+    fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    .then(response=>response.json())
+    .then(data=>console.log(data))
+  }
+
   return(
     <div className='App'>
       <form className='search-form'>
